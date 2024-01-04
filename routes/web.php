98672170f4feb6320e372847adf4d0682c23ad66
nouraa17 <nouranana31@gmail.com>
2024-01-04 02:03:11 +0200

    auth & mobile registration and login

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Controller;
 
+Auth::routes(['verify' => true]);
+
 
 /*
 |--------------------------------------------------------------------------
@@ -21,32 +23,31 @@ Route::get('/', function () {
 
 ////////////////////////////////////////////////////////////////////////////////
 // Task8
-// Route::get('/guarderbase',function(){
-//     return view('guarder');
-// })->name('guarder');
 
-// Route::get('/guarderabout',function(){
-//     return view('about');
-// })->name('about');
+// Route::get('guarderbase', [Controller::class, 'guarderbase'])->name('guarder');
 
-// Route::get('/guarderservice',function(){
-//     return view('services');
-// })->name('services');
+// Route::get('guarderabout', [Controller::class, 'guarderabout'])->name('about');
 
-// Route::get('/guarderguards',function(){
-//     return view('guards');
-// })->name('guards');
+// Route::get('guarderservice', [Controller::class, 'guarderservice'])->name('services');
 
-// Route::get('/guardercontact',function(){
-//     return view('contact');
-// })->name('contact');
+// Route::get('guarderguards', [Controller::class, 'guarderguards'])->name('guards');
 
-Route::get('guarderbase', [Controller::class, 'guarderbase'])->name('guarder');
+// Route::get('guardercontact', [Controller::class, 'guardercontact'])->name('contact');
 
-Route::get('guarderabout', [Controller::class, 'guarderabout'])->name('about');
+////////////////////////////////////////////////////////////////////////////////////////
+//Task11
 
-Route::get('guarderservice', [Controller::class, 'guarderservice'])->name('services');
 
-Route::get('guarderguards', [Controller::class, 'guarderguards'])->name('guards');
+// Auth::routes();
 
-Route::get('guardercontact', [Controller::class, 'guardercontact'])->name('contact');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('guarderbase', [Controller::class, 'guarderbase'])->middleware('verified')->name('guarder');
+
+Route::get('guarderabout', [Controller::class, 'guarderabout'])->middleware('verified')->name('about');
+
+Route::get('guarderservice', [Controller::class, 'guarderservice'])->middleware('verified')->name('services');
+
+Route::get('guarderguards', [Controller::class, 'guarderguards'])->middleware('verified')->name('guards');
+
+Route::get('guardercontact', [Controller::class, 'guardercontact'])->middleware('verified')->name('contact');
